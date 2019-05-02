714951339060f45b237124a5a20421db3ccccd4f
Mateja Lasan <mateja@lasan.ca>
2019-05-02 13:50:15 -0400

    Urgent fixes.

--- website/storage.py
+++ website/storage.py
@@ -4,10 +4,10 @@ from django.conf import settings
 
 class StaticStorage(S3Boto3Storage):
     location = 'static'
-    custom_domain = settings.CDN_URL
+    custom_domain = settings.CDN_DOMAIN_NAME
 
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
-    custom_domain = settings.CDN_URL
+    custom_domain = settings.CDN_DOMAIN_NAME
     file_overwrite = False
